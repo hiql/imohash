9f1d3d925087c33e0991eb82713b822dd6fb0630
hiql <dev.hiql@icloud.com>
2024-08-27 19:54:35 +0800

    Fix bug when file size is too small for sample size

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,7 @@ impl Hasher {
     /// Hashs a byte slice.
     pub fn sum(&self, data: &[u8]) -> Result<u128> {
         let mut reader = BufReader::new(Cursor::new(data));
-        Ok(self.hash(&mut reader)?)
+        self.hash(&mut reader)
     }
 
     /// Hashs a file.
@@ -48,7 +48,7 @@ impl Hasher {
         let path_os_string = path_canonicalized.as_os_str();
         let f = File::open(path_os_string)?;
         let mut reader = BufReader::new(f);
-        Ok(self.hash(&mut reader)?)
+        self.hash(&mut reader)
     }
 
     fn hash<R>(&self, reader: &mut R) -> Result<u128>
@@ -58,17 +58,20 @@ impl Hasher {
         let mut buffer: Vec<u8> = Vec::new();
         let size = reader.seek(SeekFrom::End(0))?;
         reader.rewind()?;
-        if size < self.sample_threshold as u64 || self.sample_size < 1u32 {
+        if self.sample_size < 1
+            || size < self.sample_threshold as u64
+            || size < (2 * self.sample_size - 1) as u64
+        {
             reader.read_to_end(&mut buffer)?;
         } else {
             let mut first_buf = vec![0u8; self.sample_size as usize];
-            reader.read(first_buf.as_mut_slice())?;
+            reader.read_exact(first_buf.as_mut_slice())?;
             reader.seek(SeekFrom::Start(size / 2))?;
             let mut middle_buf = vec![0u8; self.sample_size as usize];
-            reader.read(middle_buf.as_mut_slice())?;
+            reader.read_exact(middle_buf.as_mut_slice())?;
             reader.seek(SeekFrom::End(-(self.sample_size as i64)))?;
             let mut last_buf = vec![0u8; self.sample_size as usize];
-            reader.read(last_buf.as_mut_slice())?;
+            reader.read_exact(last_buf.as_mut_slice())?;
             buffer.append(&mut first_buf);
             buffer.append(&mut middle_buf);
             buffer.append(&mut last_buf);
@@ -77,6 +80,12 @@ impl Hasher {
         let mut hash_bytes = hash_result.rotate_right(64).swap_bytes().to_le_bytes();
         put_uvarint(&mut hash_bytes, size);
         Ok(u128::from_le_bytes(hash_bytes))
+    }
+}
+
+impl Default for Hasher {
+    fn default() -> Self {
+        Hasher::new()
     }
 }
 
@@ -98,6 +107,7 @@ mod tests {
 
     use super::*;
     use md5::{Digest, Md5};
+    use std::{fs, path::PathBuf};
 
     #[test]
     fn test_put_uvarint() {
@@ -108,14 +118,129 @@ mod tests {
         assert_eq!(buffer, expected);
     }
 
+    fn test_data_file_path(name: &str) -> String {
+        let test_data_dir = "test_data";
+
+        if !Path::new(test_data_dir).exists() {
+            fs::create_dir(test_data_dir).unwrap();
+        }
+
+        let mut path = PathBuf::new();
+        path.push(test_data_dir);
+        path.push(name);
+        return path.to_str().unwrap().to_string();
+    }
+
+    #[test]
+    fn test_default() {
+        let sample_file = test_data_file_path("default_sample");
+        let data = vec![100, 131071, 131072, 50000];
+
+        for size in data {
+            let custom_hasher: Hasher = Hasher::with_sample_size_and_threshold(16384, 131072);
+            let default_hasher: Hasher = Hasher::new();
+            let test_data: Vec<u8> = m(size);
+            assert_eq!(
+                default_hasher.sum(&test_data).unwrap(),
+                custom_hasher.sum(&test_data).unwrap()
+            );
+
+            let empty_byte_array: Vec<u8> = Vec::new();
+            fs::write(&sample_file, empty_byte_array).unwrap();
+            let h1 = default_hasher.sum_file(&sample_file).unwrap();
+            let h2 = custom_hasher.sum_file(&sample_file).unwrap();
+            assert_eq!(h1, h2);
+        }
+    }
+
     #[test]
     fn test_custom() {
-        let hasher = Hasher::with_sample_size_and_threshold(3, 45);
-        let actual = hasher.sum("hello".as_bytes()).unwrap();
+        let sample_file = test_data_file_path("sample");
+        let sample_size: u32 = 3;
+        let sample_threshold: u32 = 45;
+
+        let custom_hasher = Hasher::with_sample_size_and_threshold(sample_size, sample_threshold);
+
+        // empty file
+        let empty_byte_array: Vec<u8> = Vec::new();
+        fs::write(&sample_file, empty_byte_array).unwrap();
+        let hash = custom_hasher.sum_file(&sample_file).unwrap();
+        let empty_byte_array: Vec<u8> = vec![0; 16];
+        assert_eq!(hash.to_le_bytes(), &empty_byte_array[..]);
+
+        // small file
+        fs::write(&sample_file, b"hello").unwrap();
+        let hash = custom_hasher.sum_file(&sample_file).unwrap();
         assert_eq!(
-            "05d8a7b341bd9b025b1e906a48ae1d19",
-            hex::encode(actual.to_le_bytes())
+            hex::encode(hash.to_le_bytes()),
+            "05d8a7b341bd9b025b1e906a48ae1d19"
         );
+
+        /* boundary tests using the custom sample size */
+        let size = sample_threshold;
+
+        // test that changing the gaps between sample zones does not affect the hash
+        let mut data: Vec<u8> = vec![b'A'; size as usize];
+        fs::write(&sample_file, &data[..]).unwrap();
+        let h1 = custom_hasher.sum_file(&sample_file).unwrap();
+
+        data[sample_size as usize] = b'B';
+        data[(size - sample_size - 1) as usize] = b'B';
+        fs::write(&sample_file, &data[..]).unwrap();
+        let h2 = custom_hasher.sum_file(&sample_file).unwrap();
+        assert_eq!(h1, h2);
+
+        // test that changing a byte on the edge (but within) a sample zone
+        // does change the hash
+        let mut data: Vec<u8> = vec![b'A'; size as usize];
+        data[sample_size as usize - 1] = b'B';
+        fs::write(&sample_file, &data[..]).unwrap();
+        let h3 = custom_hasher.sum_file(&sample_file).unwrap();
+        assert_ne!(h1, h3);
+
+        let mut data: Vec<u8> = vec![b'A'; size as usize];
+        data[size as usize / 2] = b'B';
+        fs::write(&sample_file, &data[..]).unwrap();
+        let h4 = custom_hasher.sum_file(&sample_file).unwrap();
+        assert_ne!(h1, h4);
+        assert_ne!(h3, h4);
+
+        let mut data: Vec<u8> = vec![b'A'; size as usize];
+        data[(size / 2 + sample_size - 1) as usize] = b'B';
+        fs::write(&sample_file, &data[..]).unwrap();
+        let h5 = custom_hasher.sum_file(&sample_file).unwrap();
+        assert_ne!(h1, h5);
+        assert_ne!(h3, h5);
+        assert_ne!(h4, h5);
+
+        let mut data: Vec<u8> = vec![b'A'; size as usize];
+        data[(size - sample_size) as usize] = b'B';
+        fs::write(&sample_file, &data[..]).unwrap();
+        let h6 = custom_hasher.sum_file(&sample_file).unwrap();
+        assert_ne!(h1, h6);
+        assert_ne!(h3, h6);
+        assert_ne!(h4, h6);
+        assert_ne!(h5, h6);
+
+        // test that changing the size changes the hash
+        let data: Vec<u8> = vec![b'A'; size as usize + 1];
+        fs::write(&sample_file, &data[..]).unwrap();
+        let h7 = custom_hasher.sum_file(&sample_file).unwrap();
+        assert_ne!(h1, h7);
+        assert_ne!(h3, h7);
+        assert_ne!(h4, h7);
+        assert_ne!(h5, h7);
+        assert_ne!(h6, h7);
+
+        // test sampleSize < 1
+        let hasher = Hasher::with_sample_size_and_threshold(0, size);
+        let data: Vec<u8> = vec![b'A'; size as usize];
+        fs::write(&sample_file, &data[..]).unwrap();
+        let hash = hasher.sum_file(&sample_file).unwrap();
+        assert_eq!(
+            hex::encode(hash.to_le_bytes()),
+            "2d9123b54d37e9b8f94ab37a7eca6f40"
+        )
     }
 
     #[test]
@@ -139,15 +264,17 @@ mod tests {
             (16384, 131073, 131072, "808008282d3f3b53e1fd132cc51fcc1d"),
             (16384, 131072, 500000, "a0c21e44a0ba3bddee802a9d1c5332ca"),
             (50, 131072, 300000, "e0a712edd8815c606344aed13c44adcf"),
+            (0, 100, 1000, "e80753211a57ee0de67c756e98e00496"),
+            (50, 9999, 1000, "e80753211a57ee0de67c756e98e00496"),
+            (501, 20, 1000, "e80753211a57ee0de67c756e98e00496"),
+            (501, 20, 1001, "e9079899cffb46f60c8645a01f12f9c9"),
         ];
 
         for test in tests {
             let hasher = Hasher::with_sample_size_and_threshold(test.0, test.1);
             let content = m(test.2 as usize);
-            let hash_str = format!(
-                "{}",
-                hex::encode(hasher.sum(content.as_slice()).unwrap().to_le_bytes())
-            );
+            let hash_str =
+                hex::encode(hasher.sum(content.as_slice()).unwrap().to_le_bytes()).to_owned();
             assert_eq!(test.3, hash_str);
         }
     }
